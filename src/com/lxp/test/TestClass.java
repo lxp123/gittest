e0539f446b7b3277b3e0ca7226b9bd4bf6043704
liuxp <974986454@qq.com>
2015-06-08 15:44:52 +0800

    hello

---
+++ src/com/lxp/test/TestClass.java
@@ -0,0 +1,9 @@
+package com.lxp.test;
+
+public class TestClass {
+		public static void main(String[] args) {
+			System.out.println("this is my first github test");
+			System.out.println("hello github!");
+		}
+
+}
